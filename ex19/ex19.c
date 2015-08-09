1e66d7764a017e39bd3021ca8bb82e80079d6c06
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2015-08-09 16:37:29 -0700

    Ex. 19 test script

--- ex19/ex19.c
+++ ex19/ex19.c
@@ -3,213 +3,226 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <assert.h>
 #include "ex19.h"
 
 
-int Monster_attack(void *self, int damage)
-{
-    Monster *monster = self;
+int Monster_attack(void *self, int damage) {
+  assert(self != NULL);
+  assert(damage >= 0);
 
-    printf("You attack %s!\n", monster->_(description));
+  Monster *monster = self;
 
-    monster->hit_points -= damage;
+  printf("You attack %s!\n", monster->_(description));
 
-    if(monster->hit_points > 0) {
-        printf("It is still alive.\n");
-        return 0;
-    } else {
-        printf("It is dead!\n");
-        return 1;
-    }
+  monster->hit_points -= damage;
+
+  if(monster->hit_points > 0) {
+    printf("It is still alive.\n");
+    return 0;
+  } else {
+    printf("It is dead!\n");
+    return 1;
+  }
 }
 
-int Monster_init(void *self)
-{
-    Monster *monster = self;
-    monster->hit_points = 10;
-    return 1;
+int Monster_init(void *self) {
+  assert(self != NULL);
+
+  Monster *monster = self;
+  monster->hit_points = 10;
+  return 1;
 }
 
 Object MonsterProto = {
-    .init = Monster_init,
-    .attack = Monster_attack
+  .init = Monster_init,
+  .attack = Monster_attack
 };
 
 
-void *Room_move(void *self, Direction direction)
-{
-    Room *room = self;
-    Room *next = NULL;
+void *Room_move(void *self, Direction direction) {
+  assert(self != NULL);
 
-    if(direction == NORTH && room->north) {
-        printf("You go north, into:\n");
-        next = room->north;
-    } else if(direction == SOUTH && room->south) {
-        printf("You go south, into:\n");
-        next = room->south;
-    } else if(direction == EAST && room->east) {
-        printf("You go east, into:\n");
-        next = room->east;
-    } else if(direction == WEST && room->west) {
-        printf("You go west, into:\n");
-        next = room->west;
-    } else {
-        printf("You can't go that direction.");
-        next = NULL;
-    }
+  Room *room = self;
+  Room *next = NULL;
 
-    if(next) {
-        next->_(describe)(next);
-    }
+  if(direction == NORTH && room->north) {
+    printf("You go north, into:\n");
+    next = room->north;
+  } else if(direction == SOUTH && room->south) {
+    printf("You go south, into:\n");
+    next = room->south;
+  } else if(direction == EAST && room->east) {
+    printf("You go east, into:\n");
+    next = room->east;
+  } else if(direction == WEST && room->west) {
+    printf("You go west, into:\n");
+    next = room->west;
+  } else {
+    printf("You can't go that direction.");
+    next = NULL;
+  }
 
-    return next;
+  if(next) {
+    next->_(describe)(next);
+  }
+
+  return next;
 }
 
 
-int Room_attack(void *self, int damage)
-{
-    Room *room = self;
-    Monster *monster = room->bad_guy;
+int Room_attack(void *self, int damage) {
+  assert(self != NULL);
+  assert(damage >= 0);
 
-    if(monster) {
-        monster->_(attack)(monster, damage);
-        return 1;
-    } else {
-        printf("You flail in the air at nothing. Idiot.\n");
-        return 0;
-    }
+  Room *room = self;
+  Monster *monster = room->bad_guy;
+
+  if(monster) {
+    monster->_(attack)(monster, damage);
+    return 1;
+  } else {
+    printf("You flail in the air at nothing. Idiot.\n");
+    return 0;
+  }
 }
 
 
 Object RoomProto = {
-    .move = Room_move,
-    .attack = Room_attack
+  .move = Room_move,
+  .attack = Room_attack
 };
 
 
-void *Map_move(void *self, Direction direction)
-{
-    Map *map = self;
-    Room *location = map->location;
-    Room *next = NULL;
+void *Map_move(void *self, Direction direction) {
+  assert(self != NULL);
 
-    next = location->_(move)(location, direction);
+  Map *map = self;
+  Room *location = map->location;
+  Room *next = NULL;
 
-    if(next) {
-        map->location = next;
-    }
+  next = location->_(move)(location, direction);
 
-    return next;
+  if(next) {
+    map->location = next;
+  }
+
+  return next;
 }
 
-int Map_attack(void *self, int damage)
-{
-    Map* map = self;
-    Room *location = map->location;
+int Map_attack(void *self, int damage) {
+  assert(self != NULL);
+  assert(damage >= 0);
 
-    return location->_(attack)(location, damage);
+  Map* map = self;
+  Room *location = map->location;
+
+  return location->_(attack)(location, damage);
 }
 
 
-int Map_init(void *self)
-{
-    Map *map = self;
+int Map_init(void *self) {
+  assert(self != NULL);
 
-    // make some rooms for a small map
-    Room *hall = NEW(Room, "The great Hall");
-    Room *throne = NEW(Room, "The throne room");
-    Room *arena = NEW(Room, "The arena, with the minotaur");
-    Room *kitchen = NEW(Room, "Kitchen, you have the knife now");
+  Map *map = self;
 
-    // put the bad guy in the arena
-    arena->bad_guy = NEW(Monster, "The evil minotaur");
+  // make some rooms for a small map
+  Room *hall = NEW(Room, "The great Hall");
+  Room *throne = NEW(Room, "The throne room");
+  Room *arena = NEW(Room, "The arena, with the minotaur");
+  Room *kitchen = NEW(Room, "Kitchen, you have the knife now");
 
-    // setup the map rooms
-    hall->north = throne;
+  // put the bad guy in the arena
+  arena->bad_guy = NEW(Monster, "The evil minotaur");
 
-    throne->west = arena;
-    throne->east = kitchen;
-    throne->south = hall;
+  // setup the map rooms
+  hall->north = throne;
 
-    arena->east = throne;
-    kitchen->west = throne;
+  throne->west = arena;
+  throne->east = kitchen;
+  throne->south = hall;
 
-    // start the map and the character off in the hall
-    map->start = hall;
-    map->location = hall;
+  arena->east = throne;
+  kitchen->west = throne;
 
-    return 1;
+  // start the map and the character off in the hall
+  map->start = hall;
+  map->location = hall;
+
+  return 1;
 }
 
 Object MapProto = {
-    .init = Map_init,
-    .move = Map_move,
-    .attack = Map_attack
+  .init = Map_init,
+  .move = Map_move,
+  .attack = Map_attack
 };
 
 int process_input(Map *game)
 {
-    printf("\n> ");
+  printf("\n> ");
 
-    char ch = getchar();
-    getchar(); // eat ENTER
+  char ch = getchar();
+  int c = getchar(); // eat ENTER
 
-    int damage = rand() % 4;
+  assert(c != EOF);
 
-    switch(ch) {
-        case -1:
-            printf("Giving up? You suck.\n");
-            return 0;
-            break;
+  int damage = rand() % 4;
 
-        case 'n':
-            game->_(move)(game, NORTH);
-            break;
+  switch(ch) {
+    case -1:
+      printf("Giving up? You suck.\n");
+      return 0;
+      break;
 
-        case 's':
-            game->_(move)(game, SOUTH);
-            break;
+    case 'n':
+      game->_(move)(game, NORTH);
+      break;
 
-        case 'e':
-            game->_(move)(game, EAST);
-            break;
+    case 's':
+      game->_(move)(game, SOUTH);
+      break;
 
-        case 'w':
-            game->_(move)(game, WEST);
-            break;
+    case 'e':
+      game->_(move)(game, EAST);
+      break;
 
-        case 'a':
+    case 'w':
+      game->_(move)(game, WEST);
+      break;
 
-            game->_(attack)(game, damage);
-            break;
-        case 'l':
-            printf("You can go:\n");
-            if(game->location->north) printf("NORTH\n");
-            if(game->location->south) printf("SOUTH\n");
-            if(game->location->east) printf("EAST\n");
-            if(game->location->west) printf("WEST\n");
-            break;
+    case 'a':
 
-        default:
-            printf("What?: %d\n", ch);
-    }
+      game->_(attack)(game, damage);
+      break;
+    case 'l':
+      printf("You can go:\n");
+      if(game->location->north) printf("NORTH\n");
+      if(game->location->south) printf("SOUTH\n");
+      if(game->location->east) printf("EAST\n");
+      if(game->location->west) printf("WEST\n");
+      break;
 
-    return 1;
+    default:
+      printf("What?: %d\n", ch);
+  }
+
+  return 1;
 }
 
 int main(int argc, char *argv[])
 {
-    // simple way to setup the randomness
-    srand(time(NULL));
+  // simple way to setup the randomness
+  srand(time(NULL));
 
-    // make our map to work with
-    Map *game = NEW(Map, "The Hall of the Minotaur.");
+  // make our map to work with
+  Map *game = NEW(Map, "The Hall of the Minotaur.");
 
-    printf("You enter the ");
-    game->location->_(describe)(game->location);
+  printf("You enter the ");
+  game->location->_(describe)(game->location);
 
-    while(process_input(game)) {
-    }
+  while(process_input(game)) {
+  }
 
-    return 0;
+  return 0;
 }
